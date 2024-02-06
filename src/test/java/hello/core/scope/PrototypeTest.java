3861eba6a99600385b88acddfe2897eaafb087ed
injongjeon <jeonij23@gmail.com>
2024-02-06 22:19:08 +0900

    feat: final

---
+++ src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,36 @@
+package hello.core.scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+public class PrototypeTest {
+
+    @Test
+    void prototypeBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        PrototypeBean prototype1 = ac.getBean(PrototypeBean.class);
+        PrototypeBean prototype2 = ac.getBean(PrototypeBean.class);
+        System.out.println("prototype1 = " + prototype1);
+        System.out.println("prototype2 = " + prototype2);
+        Assertions.assertThat(prototype1).isNotSameAs(prototype2);
+
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+
+}
--- src/test/java/hello/core/scope/singletonBeanFind.java
+++ src/test/java/hello/core/scope/singletonBeanFind.java
@@ -2,6 +2,7 @@ package hello.core.scope;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
@@ -15,6 +16,7 @@ public class singletonBeanFind {
         SingletonBean singletonBean2 = ac.getBean(SingletonBean.class);
         System.out.println("singletonBean1 = " + singletonBean1);
         System.out.println("singletonBean2 = " + singletonBean2);
+        Assertions.assertThat(singletonBean1).isSameAs(singletonBean2);
 
 
     }
